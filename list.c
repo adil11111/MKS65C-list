332a9e5e4dce3320eb5653d080110190a63a7a2d
Adil Gondal <agondal@stuy.edu>
2018-10-10 14:00:57 -0400

    print_list done

--- list.c
+++ list.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
 
+
+
+void print_list(struct node * startP){
+  struct node * curP = startP;
+  while(curP -> next){
+    printf("%d\n",(*curP).num);
+    curP = (*curP).next;
+  }
+  printf("%d\n",(*curP).num);
+}
 
 int main(){
-  struct node { int num; struct node *next;};
-  struct node first;
-  malloc(sizeof(first));
-  first.num=0;
+  struct node *firstP = malloc(sizeof(struct node));
+  firstP -> num = 78;
+  //printf("%d\n",(firstP -> num));
   
-  struct node second;
-  malloc(sizeof(second));
-  struct node *p=&second;
-  first.next=p;
+  struct node *secondP = malloc(sizeof(struct node));
+  secondP -> num = 2;
+  firstP ->next= secondP;
+  secondP -> next = NULL;
+  //printf("%d\n",(firstP->next->num));
+  //printf("%d\n",(secondP->num));
+  print_list(firstP);
   return 0;
 }
